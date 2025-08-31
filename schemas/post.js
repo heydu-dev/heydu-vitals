e46d8a13c40ddd0df2d884a7900b02ca5529bab1
Siddhartha Vibhu Pharswan <{ID}+{username}@users.noreply.github.com>
2025-09-01 02:49:58 +0530

    changes for schema

--- schemas/post.js
+++ schemas/post.js
@@ -1,114 +1,170 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const Joi = require("joi");
+const Joi = require('joi');
 
 const PostSchema = Joi.object({
-    description: Joi.string().max(300).allow("").allow(null),
-    media: Joi.array().items({
-        key: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-        rootFolder: Joi.string().valid("posts").required(),
-        type: Joi.string().valid("video", "image").required(),
-    }).default([]).required(),
-    circleType: Joi.string().required(),
-    postUserID: Joi.string().required(), // User ID of the person who created the post: Joi.number().valid(0, 1, 2, 3).required(),
-    postUserTypeID: Joi.number().required(),
-    postUserEmail: Joi.string().email().required(),
-    postUserRoleID: Joi.number().required(),
-    institutionID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    specialisationID: Joi.string().allow("*").when("specialisationYear", {
-        is: Joi.exist(),
-        then: Joi.string().allow("*").required().allow(null),
-    }),
-    specialisationYear: Joi.string().allow(null),
+	description: Joi.string().max(300).allow('').allow(null),
+	media: Joi.array()
+		.items({
+			key: Joi.string()
+				.guid({ version: ['uuidv4', 'uuidv5'] })
+				.required(),
+			rootFolder: Joi.string().valid('posts').required(),
+			type: Joi.string().valid('video', 'image').required(),
+		})
+		.default([])
+		.required(),
+	circleType: Joi.string().required(),
+	postUserID: Joi.string().required(), // User ID of the person who created the post: Joi.number().valid(0, 1, 2, 3).required(),
+	postUserTypeID: Joi.number().required(),
+	postUserEmail: Joi.string().email().required(),
+	postUserRoleID: Joi.number().required(),
+	institutionID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	specialisationID: Joi.string()
+		.allow('*')
+		.when('specialisationYear', {
+			is: Joi.exist(),
+			then: Joi.string().allow('*').required().allow(null),
+		})
+		.allow(''),
+	specialisationYear: Joi.string().allow(null).allow(''),
 });
 
 const EditPostSchema = Joi.object({
-    description: Joi.string().max(300).allow("").allow(null),
-    media: Joi.array().items({
-        key: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-        rootFolder: Joi.string().valid("posts").required(),
-        type: Joi.string().valid("video", "image").required(),
-    }).default([]),
-    isHidden: Joi.boolean().optional(),
+	description: Joi.string().max(300).allow('').allow(null),
+	media: Joi.array()
+		.items({
+			key: Joi.string()
+				.guid({ version: ['uuidv4', 'uuidv5'] })
+				.required(),
+			rootFolder: Joi.string().valid('posts').required(),
+			type: Joi.string().valid('video', 'image').required(),
+		})
+		.default([]),
+	isHidden: Joi.boolean().optional(),
 });
 
 const DeletePostSchema = Joi.object({
-    deletedByUserRoleID: Joi.number().required(),
+	deletedByUserRoleID: Joi.number().required(),
 });
 
 const GetPutObjectSignedUrlSchema = Joi.object({
-    keys: Joi.array().items(Joi.object({
-        institutionID: Joi.number().required(),
-        profileTypeID: Joi.number().valid(0, 1, 2, 3).required(),
-        profileEmail: Joi.string().email().required(),
-        key: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    })).required().min(1),
+	keys: Joi.array()
+		.items(
+			Joi.object({
+				institutionID: Joi.number().required(),
+				profileTypeID: Joi.number().valid(0, 1, 2, 3).required(),
+				profileEmail: Joi.string().email().required(),
+				key: Joi.string()
+					.guid({ version: ['uuidv4', 'uuidv5'] })
+					.required(),
+			}),
+		)
+		.required()
+		.min(1),
 });
 
 const GetPostSchema = Joi.object({
-    profileUserID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    limit: Joi.number().required().max(20),
-    lastEvaluatedKey: Joi.alternatives().try(Joi.object().unknown(true), Joi.allow(null)),
-    isFollowing: Joi.boolean(),
-    toFollowers: Joi.boolean(),
-    specialisationID: Joi.string().allow("*").when("specialisationYear", {
-        is: Joi.exist(),
-        then: Joi.string().allow("*").required(),
-    }),
-    specialisationYear: Joi.string().allow("*"),
-    institutionID: Joi.string(),
+	profileUserID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	limit: Joi.number().required().max(20),
+	lastEvaluatedKey: Joi.alternatives().try(
+		Joi.object().unknown(true),
+		Joi.allow(null),
+	),
+	isFollowing: Joi.boolean(),
+	toFollowers: Joi.boolean(),
+	specialisationID: Joi.string()
+		.allow('*')
+		.when('specialisationYear', {
+			is: Joi.exist(),
+			then: Joi.string().allow('*').required(),
+		}),
+	specialisationYear: Joi.string().allow('*'),
+	institutionID: Joi.string(),
 });
 
 const GetProfilePostSchema = Joi.object({
-    profileUserID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    viewProfileUserID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    limit: Joi.number().required().max(20),
-    lastEvaluatedKey: Joi.alternatives().try(Joi.object().unknown(true), Joi.allow(null)),
+	profileUserID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	viewProfileUserID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	limit: Joi.number().required().max(20),
+	lastEvaluatedKey: Joi.alternatives().try(
+		Joi.object().unknown(true),
+		Joi.allow(null),
+	),
 });
 
 const LikePostSchema = Joi.object({
-    postID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    likedBy: Joi.string().email().required(),
+	postID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	likedBy: Joi.string().email().required(),
 });
 
 const GetLikedPostSchema = Joi.object({
-    profileUserID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    limit: Joi.number().required().max(20),
-    lastEvaluatedKey: Joi.alternatives().try(Joi.object().unknown(true), Joi.allow(null)),
+	profileUserID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	limit: Joi.number().required().max(20),
+	lastEvaluatedKey: Joi.alternatives().try(
+		Joi.object().unknown(true),
+		Joi.allow(null),
+	),
 });
 
 const SavePostSchema = Joi.object({
-    postID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    savedBy: Joi.string().email().required(),
+	postID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	savedBy: Joi.string().email().required(),
 });
 
 const GetSavedPostSchema = Joi.object({
-    profileUserID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    limit: Joi.number().required().max(20),
-    lastEvaluatedKey: Joi.alternatives().try(Joi.object().unknown(true), Joi.allow(null)),
+	profileUserID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	limit: Joi.number().required().max(20),
+	lastEvaluatedKey: Joi.alternatives().try(
+		Joi.object().unknown(true),
+		Joi.allow(null),
+	),
 });
 
 const PinPostSchema = Joi.object({
-    postID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    pinnedBy: Joi.string().email().required(),
+	postID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	pinnedBy: Joi.string().email().required(),
 });
 
 const GetPinnedPostSchema = Joi.object({
-    institutionID: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-    limit: Joi.number().required().max(20),
-    lastEvaluatedKey: Joi.alternatives().try(Joi.object().unknown(true), Joi.allow(null)),
+	institutionID: Joi.string()
+		.guid({ version: ['uuidv4', 'uuidv5'] })
+		.required(),
+	limit: Joi.number().required().max(20),
+	lastEvaluatedKey: Joi.alternatives().try(
+		Joi.object().unknown(true),
+		Joi.allow(null),
+	),
 });
 
 module.exports = {
-    PostSchema,
-    EditPostSchema,
-    DeletePostSchema,
-    GetPutObjectSignedUrlSchema,
-    GetPostSchema,
-    GetProfilePostSchema,
-    LikePostSchema,
-    GetLikedPostSchema,
-    SavePostSchema,
-    GetSavedPostSchema,
-    PinPostSchema,
-    GetPinnedPostSchema
+	PostSchema,
+	EditPostSchema,
+	DeletePostSchema,
+	GetPutObjectSignedUrlSchema,
+	GetPostSchema,
+	GetProfilePostSchema,
+	LikePostSchema,
+	GetLikedPostSchema,
+	SavePostSchema,
+	GetSavedPostSchema,
+	PinPostSchema,
+	GetPinnedPostSchema,
 };
